2dd05cca1207f1d5059530436991fd7de96c6e9a
ElioiMusk <elioteuber@outlook.com>
2022-11-06 23:46:11 +0100

    feat(sound): adding background music with transition

--- src/BulletData.java
+++ src/BulletData.java
@@ -7,8 +7,9 @@ public class BulletData {
     int explosionAnimatonFrameCount;
     int coolDown;
     int delay;
+    int volumeOffset;
 
-    public BulletData(int speed, int damage, int size, int explosionSize, int explosionDamage, int explosionAnimatonFrameCount, int coolDown, int delay) {
+    public BulletData(int speed, int damage, int size, int explosionSize, int explosionDamage, int explosionAnimatonFrameCount, int coolDown, int delay, int volumeOffset) {
         this.speed = speed;
         this.damage = damage;
         this.size = size;
@@ -17,20 +18,21 @@ public class BulletData {
         this.explosionAnimatonFrameCount = explosionAnimatonFrameCount;
         this.coolDown = coolDown;
         this.delay = delay;
+        this.volumeOffset = volumeOffset;
     }
 
     // TODO: Balance Bullets
     public static BulletData[] bullets = new BulletData[]{
 //            Bullet type 1 (Default)
-            new BulletData(8, 100, 15, 30, 10, 6, 0, 50),
+            new BulletData(8, 100, 15, 30, 10, 6, 0, 50, -5),
 //            Bullet type 2 (Rocket)
-            new BulletData(10, 150, 50, 50, 100, 16, 20, 50),
+            new BulletData(10, 150, 50, 50, 100, 16, 20, 50, -10),
 //            Bullet type 3 (Bomb)
-            new BulletData(7, 20, 40, 80, 300, 15, 10, 50),
+            new BulletData(7, 20, 40, 80, 300, 15, 10, 50, -5),
 //            Bullet type 4 (Missile)
-            new BulletData(6, 20, 30, 80, 200, 30, 30, 70),
+            new BulletData(6, 20, 30, 80, 200, 30, 30, 70, -15),
 //            Bullet type 5 (Nuke)
-            new BulletData(5, 20, 90, 90, 200, 23, 10, 50)};
+            new BulletData(5, 20, 90, 90, 200, 23, 10, 50, -10)};
 
     public static BulletData getBullet(int type) {
         return bullets[type - 1];
--- src/SoundService.java
+++ src/SoundService.java
@@ -1,14 +1,67 @@
 import greenfoot.GreenfootSound;
 
-public class SoundService extends GreenfootSound {
-    public SoundService(String fileName) {
-        super(fileName);
+public class SoundService {
+    int volumeOffset = 0;
+    GreenfootSound sound = new GreenfootSound("");
+    boolean loop = false;
+    String fileName;
+
+    public SoundService(String file) {
+        setSound(file);
+    }
+
+    public SoundService() {
+    }
+
+    public void setSound(String file) {
+        this.fileName = file;
+        sound = new GreenfootSound(fileName);
+        sound.setVolume(Space.volume + volumeOffset);
+    }
+
+    public void setSound() {
+        sound = new GreenfootSound(fileName);
+        sound.setVolume(Space.volume + volumeOffset);
+    }
+
+    public void setVolume() {
+        if (Space.volume <= 0) {
+            sound.setVolume(Space.volume);
+        } else {
+            sound.setVolume(Space.volume + volumeOffset);
+        }
     }
 
     public void playSound() {
-        if (Space.volume > 0) {
-            setVolume(Space.volume);
-            play();
+        setSound();
+        sound.setVolume(Space.volume + volumeOffset);
+        if (loop) {
+            sound.playLoop();
+        } else {
+            sound.play();
         }
+    }
+
+    public void playSound(String fileName) {
+        setSound(fileName);
+        if (Space.volume > 0) {
+            sound.setVolume(Space.volume + volumeOffset);
+            if (loop) {
+                sound.playLoop();
+            } else {
+                sound.play();
+            }
+        }
+    }
+
+    public void stopSound() {
+        sound.stop();
+    }
+
+    public boolean isPlaying() {
+        if (sound == null) {
+            return false;
+        }
+        return sound.isPlaying();
     }
 }
